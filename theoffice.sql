6797111b033bf8461b72f6141675ccbc6268cfcd
prajwal <prajwalpatil3764@gmail.com>
2021-06-06 13:23:40 +0530

    patterns

--- theoffice.sql
+++ theoffice.sql
@@ -125,3 +125,127 @@ INSERT INTO works_with VALUES(102, 406, 15000);
 INSERT INTO works_with VALUES(105, 406, 130000);
 
 SELECT * FROM employee;
+-- select all the employees and orderby salary
+SELECT *
+FROM employee
+ORDER BY  salary DESC ;  -- ordering the desending 
+
+-- select all the employees ordered by sex and name 
+SELECT *
+FROM employee 
+ORDER BY sex,first_name,last_namee; 
+
+-- find the first 5 emloyees in the table 
+SELECT *
+FROM employee
+LIMIT 5;
+-- find the first and last name of the employees
+SELECT first_name,last_namee
+FROM employee;
+
+-- find the forename and surnames of the all employees
+-- since the columns are named as the first_name and last_name i need to use the AS 
+SELECT first_name AS forename , last_namee AS surname -- what this is doing is it is changing the column name by using the AS 
+FROM employee;
+
+-- find all the different genders in the table 
+SELECT DISTINCT branch_name     -- this is telling all the different branch_name in the branch table by using the DISTINCT 
+FROM branch; 
+
+--------------------------------- SQL -- FUNCTIONS -------------------------------------------------------------------
+-- function is a piece of code 
+-- sql has different functions just like other programming languages 
+
+-- find the number of employees
+SELECT COUNT(emp_id) 
+FROM employee; -- here the count is sql function and it calculating the number ofemployees using the emp_id
+
+
+-- find the female employees born after 1970 
+SELECT COUNT(emp_id)
+FROM employee
+WHERE sex='f' AND birth_date>'1970-01-01';
+
+SELECT COUNT(emp_id) 
+FROM employee
+WHERE sex='m' AND birth_date>'1970-01-01';-- for male employees
+
+-- find the averages of all the employees salary 
+SELECT AVG(salary) 
+FROM employee; -- this is finding the average of all the employees through AVG()
+
+-- find the averages of all the employees salary male employees
+SELECT AVG(salary) 
+FROM employee
+WHERE sex='m';
+
+
+-- find the sum of all employees
+SELECT SUM(salary) 
+FROM employee;
+
+-- find out how many male and female workers are there 
+-- in the first line it will first print the count and second sex is written for showing sex column
+select COUNT(sex) , sex
+FROM employee
+GROUP BY(sex); -- group by saying that it should grouped by sex 
+
+-- find the total sales of each salesman     
+-- here in the below line extra emp_id is written to print the emp_id  
+SELECT emp_id, SUM(total_sales) 
+FROM works_with
+GROUP BY(emp_id); -- this gonna tell how much each employee has sold totally
+
+-- find total amount each client has spend 
+SELECT client_id, SUM(total_sales) 
+FROM works_with
+GROUP BY(client_id); -- this is showing how each client has spent 
+
+SELECT first_name, branch_id 
+FROM employee
+GROUP BY(first_name);
+
+----- patterns using the like 
+SELECT *
+FROM employee
+WHERE last_namee LIKE '%d'; -- this is giving only the last name whose ends with d and there may any number of characters before d 
+
+SELECT *
+FROM employee
+WHERE last_namee LIKE '%d%';
+ -- this is giving all the last_names who have d in their last_names irrespective where it is 
+
+SELECT *
+FROM employee
+WHERE last_namee LIKE 'm%'; -- this is giving all the last_name which starts with the m 
+
+SELECT *
+FROM employee
+WHERE last_namee LIKE '_____n'; 
+-- this will strictly only see the last names with 2 characters and that ends with y 
+-- if 5 underscore were given than it will search for 6 character last_name which ends with y
+
+SELECT *
+FROM employee
+WHERE last_namee NOT LIKE 'm%'; -- not it will give all the last names without m starting    
+
+-- we can use the regular expression 
+SELECT * 
+FROM employee 
+WHERE last_namee REGEXP '^m';
+-- ^ indicate the begining of the string 
+-- $ indicate the end of the string 
+-- | pipe this is used to search for multiple words or characters 
+-- [abcdfg] 
+-- [a-g] represent the range 
+SELECT * 
+FROM employee 
+WHERE last_namee REGEXP 'd$'; -- this is showing all the last names which ends with d 
+
+SELECT * 
+FROM employee 
+WHERE last_namee REGEXP 'a|d|r';	-- this is showing all the last names either which a or d 
+
+SELECT * 
+FROM employee 
+WHERE last_namee REGEXP '[raf]d'; -- this will try match all the last names like rd , ad , fd        since bernard has ad so 
